9103725564579abbb30cc97b9ce45f796d82f999
Tatien Miller <tatmil99@gmail.com>
2023-07-03 18:04:54 -0500

    Creates code that references DOM nodes and creates divs to fill sketchpad container based on container height and width.

---
+++ script.js
@@ -0,0 +1,17 @@
+// Create reference to sketch container node
+// Create x divs based on grid selection (16x16, a = 256);
+// Create size of grid based on height and width of container / grid selection (h = 600 / 16, w = 600 / 16)
+// Apply classname and styling to divs
+// Append to parent node
+
+const sketchpad = document.querySelector(".sketch-container");
+const containerHeight = sketchpad.offsetHeight;
+const containerWidth = sketchpad.offsetWidth;
+
+for (let i = 0; i < 256; i++) {
+  const newDiv = document.createElement("div");
+  newDiv.classList.toggle("grid");
+  newDiv.style.height = `${containerHeight / 16}px`;
+  newDiv.style.width = `${containerWidth / 16}px`;
+  sketchpad.appendChild(newDiv);
+}
